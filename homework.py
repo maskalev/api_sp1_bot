bb93f2e269d65a6ae1a90179275e5bc4ad681a50
Aleksandr Maskalev <ya.maskalev@yandex.ru>
2020-12-28 00:38:29 +0500

    Changes after the first review
    Add another variants of reviewer's verdicts.
    Add variants of verdicts in cases of any problem.
    Input verdicts in dict.
    Take current time in the case returning None from timestamp.
    Remove logging config from function.

--- homework.py
+++ homework.py
@@ -14,38 +14,75 @@ TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s, %(levelname)s, %(name)s, '
+                           '%(message)s')
+
 
 def parse_homework_status(homework):
+    """
+    Return reviewer's verdict on the current homework (rejected of approved)
+    :param homework: dict
+    :return: str
+    """
     homework_name = homework.get('homework_name')
-    if homework.get('status') == 'rejected':
-        verdict = 'К сожалению в работе нашлись ошибки.'
+    if homework_name is None:
+        return 'У работы нет имени. Обратись к ревьюверу или куратору.'
     else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+        homework_status = homework.get('status')
+        verdicts_by_statuses = {
+            'reviewing': 'Работа взята в ревью. Скоро станет известен '
+                         'результат.',
+            'rejected': 'К сожалению в работе нашлись ошибки.',
+            'approved': 'Ревьюеру всё понравилось, можно приступать к '
+                        'следующему уроку.',
+            'connection_error': 'Ошибка запроса к API Яндекс.Практикума',
+            'value_error': 'Ошибка получения словаря',
+        }
+        if homework_status in verdicts_by_statuses:
+            verdict = verdicts_by_statuses.get(homework_status)
+            return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+        return 'Проверь статус работы и обратись к ревьюверу или куратору.'
 
 
 def get_homework_statuses(current_timestamp):
+    """
+    Return json of homeworks from specified time
+    :param current_timestamp: int
+    :return: dict
+    """
+    if current_timestamp is None:
+        current_timestamp = int(time.time())
     headers = {
-        'Authorization': 'OAuth ' + PRAKTIKUM_TOKEN,
+        'Authorization': f'OAuth {PRAKTIKUM_TOKEN}',
     }
     params = {
         'from_date': current_timestamp,
     }
-    homework_statuses = requests.get(URL, headers=headers, params=params)
-    return homework_statuses.json()
+    connection_error_dict = {'homeworks': [{'status': 'connection_error'}]}
+    value_error_dict = {'homeworks': [{'status': 'value_error'}]}
+    try:
+        homework_statuses = requests.get(URL, headers=headers, params=params)
+        return homework_statuses.json()
+    except ConnectionError:
+        return connection_error_dict
+    except ValueError:
+        return value_error_dict
 
 
 def send_message(message, bot_client):
+    """
+    Send reviewer's verdict to specified Telegram chat
+    :param message: str
+    :param bot_client: telegram.bot.Bot
+    :return: telegram.message.Message
+    """
     return bot_client.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
     telegram_bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
-    logging.basicConfig(level=logging.DEBUG,
-                        format='%(asctime)s, %(levelname)s, %(name)s, '
-                               '%(message)s')
-
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
