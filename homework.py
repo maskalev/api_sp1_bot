4c0147bd39efec7af02de86c0100fcccf3148ebe
Aleksandr Maskalev <ya.maskalev@yandex.ru>
2020-12-30 07:58:42 +0500

    Changes after the second review
    Add guard_block on homework_status.
    Return an empty dict from get_homework_statuses in the
    case on the error.

--- homework.py
+++ homework.py
@@ -8,7 +8,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
@@ -28,52 +27,38 @@ def parse_homework_status(homework):
     homework_name = homework.get('homework_name')
     if homework_name is None:
         return 'У работы нет имени. Обратись к ревьюверу или куратору.'
-    else:
-        homework_status = homework.get('status')
-        verdicts_by_statuses = {
-            'reviewing': 'Работа взята в ревью. Скоро станет известен '
-                         'результат.',
-            'rejected': 'К сожалению в работе нашлись ошибки.',
-            'approved': 'Ревьюеру всё понравилось, можно приступать к '
-                        'следующему уроку.',
-            'connection_error': 'Ошибка запроса к API Яндекс.Практикума',
-            'value_error': 'Ошибка получения словаря',
-        }
-        if homework_status in verdicts_by_statuses:
-            verdict = verdicts_by_statuses.get(homework_status)
-            return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
-        return 'Проверь статус работы и обратись к ревьюверу или куратору.'
+    homework_status = homework.get('status')
+    verdicts_by_statuses = {
+        'reviewing': 'Работа взята в ревью. Скоро станет известен '
+                     'результат.',
+        'rejected': 'К сожалению в работе нашлись ошибки.',
+        'approved': 'Ревьюеру всё понравилось, можно приступать к '
+                    'следующему уроку.',
+    }
+    if verdicts_by_statuses.get(homework_status) is None:
+        return (f'Проверь статус работы "{homework_name}" и обратись к '
+                f'ревьюверу или куратору.')
+    verdict = verdicts_by_statuses.get(homework_status)
+    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
-def get_homework_statuses(current_timestamp):
+def get_homework_statuses(current_timestamp=int(time.time())):
     """
     Return json of homeworks from specified time
     :param current_timestamp: int
     :return: dict
     """
-    if current_timestamp is None:
-        current_timestamp = int(time.time())
     headers = {
         'Authorization': f'OAuth {PRAKTIKUM_TOKEN}',
     }
     params = {
         'from_date': current_timestamp,
     }
-    connection_error_dict = {'homeworks':
-                                 [{'status': 'connection_error',
-                                   'homework_name': 'current homework'}]
-                             }
-    value_error_dict = {'homeworks':
-                            [{'status': 'value_error',
-                              'homework_name': 'current homework'}]
-                        }
     try:
         homework_statuses = requests.get(URL, headers=headers, params=params)
         return homework_statuses.json()
-    except ConnectionError:
-        return connection_error_dict
-    except ValueError:
-        return value_error_dict
+    except (ValueError, ConnectionError):
+        return {}
 
 
 def send_message(message, bot_client):
@@ -98,7 +83,6 @@ def main():
             current_timestamp = new_homework.get('current_date',
                                                  current_timestamp)
             time.sleep(300)
-
         except Exception as e:
             logging.exception(msg=f'Бот столкнулся с ошибкой: {e}')
             time.sleep(5)
